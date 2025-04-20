600114bea45b195ef01cf1709148a73aa58b9f60
Miguel <toliguemi94@gmail.com>
2025-04-20 02:28:41 -0400

    broken af but somewhat wroking

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,71 @@
+import { useState } from "react";
 import "./index.css";
 import RecipeTable from "./table";
 function App() {
+  type Recipe = {
+    name: string;
+    description?: string;
+    ingredients: {
+      name: string;
+      proportion: number;
+    }[];
+  };
+
+  const masaMadreRecipe: Recipe = {
+    name: "Pan de masa madre",
+    description: "Idealmente fermentar un minimo de 24 horas en frío",
+    ingredients: [
+      { name: "Harina de fuerza", proportion: 0.85 },
+      { name: "Harina integral", proportion: 0.15 },
+      { name: "Agua", proportion: 0.7 },
+      { name: "Masa Madre", proportion: 0.1 },
+      { name: "Sal", proportion: 0.02 },
+    ],
+  };
+
+  const twentyFourHoursPizzaRecipe: Recipe = {
+    name: "24 Hour proof pizza",
+    description:
+      "Pizza dough made to proof for 24 hours in the fridge (4-5°C), a traditional pizza dough ball is around 230-260 grams",
+    ingredients: [
+      {
+        name: "Bread flour",
+        proportion: 100,
+      },
+      { name: "Water", proportion: 60 },
+      { name: "Salt", proportion: 2 },
+      { name: "Instant dry yeast", proportion: .35 },
+    ],
+  };
+
+  const recipeList = [masaMadreRecipe, twentyFourHoursPizzaRecipe];
+
+  const [activeRecipe, setActiveRecipe] = useState(0);
+
   return (
-    <>
-      <div className="p-4">
-        <h1 className="text-4xl font-extrabold text-zinc-800 mb-8">
-          Calculadora de pancitos
-        </h1>
-      
-      <RecipeTable /></div>
-    </>
+    <div className="p-4 h-screen flex flex-col">
+      <h1 className="text-4xl font-extrabold text-zinc-800 mb-8">
+        Calculadora de pancitos
+      </h1>
+      <small>
+        Puedes modificar cualquier valor y las cantidades se adaptarán
+        manteniendo la receta!
+      </small><div className="my-4">
+      <label htmlFor="recipeSelect" >Current recipe: </label>
+      <select
+      className="bg-white w-lg py-1 px-2 border-2 border-zinc-700 rounded-xl font-bold "
+        id="recipeSelect"
+        onChange={(e: any) => {
+          setActiveRecipe(e.target.value);
+        }}
+      >
+        {recipeList.map((recipe, i) => {
+          return <option key={i} value={i}>{recipe.name}</option>;
+        })}
+      </select></div>
+
+      <RecipeTable recipe={recipeList[activeRecipe]} />
+    </div>
   );
 }
 
--- src/table.tsx
+++ src/table.tsx
@@ -1,91 +1,162 @@
-import { useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 
-export default function RecipeTable() {
-  //default value
-  const totalDough = 1000;
+type Recipe = {
+  name: string;
+  description?: string;
+  ingredients: {
+    name: string;
+    proportion: number;
+  }[];
+};
+type QuantityList = { name: string; amount: number; proportion: number }[];
 
-  const defaultRecipe = {
-    name: "Pan de masa madre",
-    ingredients: [
-      { name: "Harina de fuerza", proportion: 0.85 },
-      { name: "Harina integral", proportion: 0.15 },
-      { name: "Agua", proportion: 0.7 },
-      { name: "Masa Madre", proportion: 0.1 },
-      { name: "Sal", proportion: 0.02 },
-    ],
-  };
+export default function RecipeTable(props: { recipe: Recipe }) {
+  const [calculations, setCalculations] = useState<QuantityList>([]);
+  const [formValues, setFormValues] = useState(calculations);
 
-  const totalCalculation = defaultRecipe.ingredients.reduce(
+  const totalRatio = props.recipe.ingredients.reduce(
     (acc, curr) => acc + curr.proportion,
     0
   );
 
-  //check which ratio and its set value is changing the recipe and update the state
   const [refRatio, setRefRatio] = useState({
-    lastRatio: totalCalculation,
-    lastValue: totalDough,
+    lastRatio: totalRatio,
+    lastValue: 1000,
   });
+
+  const recipe = props.recipe;
+
+
+
+  //check which ratio and its set value is changing the recipe and update the state
+
+
+  const defaultValues: QuantityList = [
+    { name: "doughWeight", amount: 1000, proportion: totalRatio },
+  ];
+
+  recipe.ingredients.forEach((ing) => {
+    defaultValues.push({
+      name: ing.name,
+      amount:
+        (Math.round(
+          (ing.proportion * refRatio.lastValue) / totalRatio + Number.EPSILON
+        ) *
+          100) /
+        100,
+      proportion: ing.proportion,
+    });
+  });
+
+  setFormValues(defaultValues);
+
+
+  console.log("Default",defaultValues);
+  console.log("Calculations",calculations);
+
+  function updateValues(lastRatio: number, lastValue: number) {
+    setRefRatio({ lastRatio, lastValue });
+    const newValues: QuantityList = [];
+    recipe.ingredients.forEach((ing) => {
+      newValues.push({
+        name: ing.name,
+        amount:
+          Math.round(
+            ((ing.proportion * lastValue) / lastRatio + Number.EPSILON) * 100
+          ) / 100,
+        proportion: ing.proportion,
+      });
+    });
+    newValues.unshift({
+      name: "doughWeight",
+      amount: (totalRatio * lastValue) / lastRatio,
+      proportion: totalRatio,
+    });
+    setCalculations(newValues);
+    setFormValues(newValues);
+  }
+
+  function updateForm(amount: number, index: number) {
+    const updatedValue = {
+      name: calculations[index].name,
+      amount: amount,
+      proportion: calculations[index].proportion,
+    };
+    setFormValues(
+      formValues
+        .slice(0, index)
+        .concat(updatedValue, formValues.slice(index + 1))
+    );
+  }
 
   return (
     <div className="text-zinc-700">
-      <h2 className="text-2xl font-extrabold">{defaultRecipe.name}</h2>
-      <p>Total calculation {totalCalculation} g.</p>
-      <p>
-        Last set ratio {refRatio.lastRatio}, value {refRatio.lastValue}
-      </p>
+      <h2 className="text-2xl font-extrabold">{recipe.name}</h2>
+      <p>{recipe.description}</p>
       <div className="flex flex-row items-baseline py-2">
         <p className="text-xl font-bold">Masa total:</p>
-        <input
-          defaultValue={
-            (refRatio.lastRatio * refRatio.lastValue) / totalCalculation
-          }
-          className="w-[4rem] border-2 border-zinc-700 mx-2 overflow-clip"
-        ></input>
+        <form
+          onSubmit={(e: any) => {
+            e.preventDefault();
+            updateValues(totalRatio, e.target.amount.value);
+          }}
+        >
+          <input
+            name="amount"
+            value={Number(Math.round(formValues[0].amount))}
+            onChange={(e: any) => updateForm(e.target.value, 0)}
+            className="w-[4rem] border-2 border-zinc-700 mx-2 overflow-clip"
+          />
+        </form>
         <p>g.</p>
       </div>
-      <table className="border-2 border-zinc-800 w-full">
-        <thead>
-          <tr className="border-zinc-800">
-            <th>Ingrediente</th>
-            <th>
-              Proporción <br />
-              (100% = Harinas)
-            </th>
-            <th>
-              Cantidad <br />
-              (gramos)
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {defaultRecipe.ingredients.map((ing) => {
-            return (
-              <tr key={ing.name}>
-                <td>{ing.name}</td>
-                <td>{ing.proportion * 100 + "%"}</td>
-                <td>
-                  <input
-                    type="number"
-                    defaultValue={Number(
-                      Math.round(
-                        (refRatio.lastValue * ing.proportion) /
-                          refRatio.lastRatio +
-                          "e+2"
-                      ) + "e-2"
-                    )}
-                    onSubmit={(e) => {
-                      setRefRatio({
-                        lastRatio: ing.proportion,
-                        lastValue: e.target.value,
-                      });
-                    }}
-                  ></input>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="border-2 border-zinc-800 max-w-[48rem] px-4 py-4">
+        <table className="w-full">
+          <thead>
+            <tr className="border-zinc-800">
+              <th>Ingrediente</th>
+              <th>
+                Proporción <br />
+                (100% = Harinas)
+              </th>
+              <th>
+                Cantidad <br />
+                (gramos)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {calculations.slice(1).map((ing, index) => {
+              return (
+                <tr key={ing.name}>
+                  <td>{ing.name}</td>
+                  <td className="flex flex-row justify-center">
+                    {ing.proportion + "%"}
+                  </td>
+                  <td>
+                    <form
+                      className="flex flex-row justify-center"
+                      onSubmit={(e: any) => {
+                        e.preventDefault();
+                        updateValues(ing.proportion, e.target.amount.value);
+                      }}
+                    >
+                      <input
+                        name="amount"
+                        type="number"
+                        value={formValues[index + 1].amount}
+                        onChange={(e: any) =>
+                          updateForm(e.target.value, index + 1)
+                        }
+                      ></input>
+                    </form>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
